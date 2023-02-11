5e4ce4682a1864bb804d60f4ff1c1f0b690996af
one-lov-e <103420553+one-lov-e@users.noreply.github.com>
2023-02-11 18:42:56 +0000

    Deploying to gh-pages from @ one-lov-e/goit-js-hw-09@3477954048fbed870cb0496612658dccb57c0f04 🚀

---
+++ 01-color-switcher.cbca8da5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));let d=null;e.setAttribute("disabled",!0);
+//# sourceMappingURL=01-color-switcher.cbca8da5.js.map
